fc0557f4701baa1bb5c8ba4026ad0db4600f0cee
tnyhfr@gmail.com <Tony@Tonys-MacBook-Air.local>
2017-09-11 09:04:14 -0700

    Checkpoint 17 Completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+    var template =
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
@@ -14,14 +15,19 @@ var collectionItemTemplate =
   + '</div>'
   ;
 
-  window.onload = function() {
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
+  return $(template);
+};
+
+  $(window).load(function() {
+
+    var $collectionContainer = $('.album-covers');
+
+    $collectionContainer.empty();
 
     // #3
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+      var $newThumbnail = buildCollectionItemTemplate();
+
+      $collectionContainer.append($newThumbnail);
     }
-}
+});
